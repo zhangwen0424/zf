a87b44813b3e90a60fb67ff184a020f0d0ba6b2e
mornki <1048515550@qq.com>
2022-11-28 18:28:25 +0800

    命名空间

--- ts/src/module/a.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// export default "a";
-// export = {
-//   a: 1,
-// };
-//# sourceMappingURL=a.js.map
--- ts/src/module/a.ts
+++ ts/src/module/a.ts
@@ -5,6 +5,7 @@
 // };
 
 export namespace Zoo {
+  export let a = "1";
   export class Dog {
     a = 1;
   }
@@ -13,4 +14,42 @@ namespace Home {
   class Dog {}
 }
 Zoo.Dog;
-// Home.Dog; //报错
+Zoo.a;
+
+// 命名空间的嵌套
+export namespace Earth {
+  export namespace Contry {
+    export class China {}
+    export class America {}
+  }
+}
+console.log(Earth.Contry.China);
+
+// 命名空间可以用于扩展类、函数、枚举。
+class A {
+  static b = "b";
+}
+namespace A {
+  export let a = "a";
+}
+A.a;
+A.b;
+
+function counter(): number {
+  return counter.count++;
+}
+// 命名空间会合并，需要放到后面
+namespace counter {
+  export let count = 0;
+}
+
+enum ROLE {
+  user,
+}
+namespace ROLE {
+  export let admin = 1;
+}
+ROLE.user;
+ROLE.admin;
+
+// 因为有了外部模块
